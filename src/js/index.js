b6a2098ceb03d4a1b71e2b4f4c2d63bd528758eb
Salomone Baquis <salomone.l.baquis@gmail.com>
2021-02-21 17:11:55 -0500

    Reset if anchor clicked

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,10 @@
 import SelectFilter from "./select-filter.js";
 import SearchInput from "./search-input.js";
 
+document
+  .querySelector(".album__current-anchor")
+  .addEventListener("click", () => {
+    document.querySelector(".album__list").dataset.filter = "all";
+  });
+
 export { SelectFilter, SearchInput };
--- src/js/select-filter.js
+++ src/js/select-filter.js
@@ -1,6 +1,8 @@
 class SelectFilter extends HTMLElement {
   constructor() {
     super();
+    this.albumListEl = document.querySelector(".album__list");
+    this.currentAlbumLinkEl = document.querySelector(".album__current-anchor");
   }
 
   connectedCallback() {
@@ -9,15 +11,15 @@ class SelectFilter extends HTMLElement {
 
   handleSelect(e) {
     const selectedOption = e.target.value;
-    document.querySelector(".album__list").dataset.filter = selectedOption;
+    this.albumListEl.dataset.filter = selectedOption;
   }
 
   render() {
     this.innerHTML = `
-      <div class="filter">
+      <div class="filter" >
         <label class="filter__label" for="filter">Filter by:</label>
         <select class="filter__select" name="filter__select" id="filter">
-          <option value="all">All</option>
+          <option value="all" selected>All</option>
           <option value="listened">Listened</option>
           <option value="unlistened">Unlistened</option>
         </select>
@@ -27,9 +29,27 @@ class SelectFilter extends HTMLElement {
     this.afterRender();
   }
 
+  reset() {
+    this.selectEl.value = "all";
+    this.albumListEl.dataset.filter = "all";
+  }
+
   afterRender() {
+    // const url = new URL(window.location.href);
+    // const searchParams = new URLSearchParams(url.search);
+    // const filterVal = searchParams.get("filter");
+
     this.selectEl = document.querySelector(".filter__select");
-    this.selectEl.addEventListener("change", this.handleSelect);
+
+    // if (filterVal && (filterVal === "listened" || filterVal === "unlistened")) {
+    //   const lowercaseVal = filterVal.toLowerCase();
+    //   this.albumListEl.dataset.filter = lowercaseVal;
+    //   this.selectEl.value = lowercaseVal;
+    // }
+
+    this.selectEl.addEventListener("change", (e) => this.handleSelect(e));
+
+    this.currentAlbumLinkEl.addEventListener("click", () => this.reset());
   }
 }
 
