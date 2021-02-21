f29424195016c0bd8cfc5ea5f3b5bc4c9090c06f
Salomone Baquis <salomone.l.baquis@gmail.com>
2021-02-21 17:28:54 -0500

    Fix for layout shift

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,6 @@
 import SelectFilter from "./select-filter.js";
 import SearchInput from "./search-input.js";
 
+document.querySelector(".filters").classList.remove("filters-placeholder");
+
 export { SelectFilter, SearchInput };
