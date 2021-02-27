676143c09f3dd1c0ba0f0a260e8dce87706dfb34
Salomone Baquis <salomone.l.baquis@gmail.com>
2021-02-26 21:16:55 -0500

    Remove old file, placeholders

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,4 @@
 import SelectFilter from "./select-filter.js";
 import SearchInput from "./search-input.js";
 
-document.querySelector(".filters").classList.remove("filters-placeholder");
-
 export { SelectFilter, SearchInput };
